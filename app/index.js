c466dc511ac70a2dee1ba190a59fd08e0c247d0f
Daví Machado <machadobatista.davi@gmail.com>
2017-07-15 00:27:39 -0300

    Updated

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const App = () => <h1>React starter...</h1>
+const App = () => <h1>React starter...</h1>;
 
 render(<App />, document.querySelector('#root'));
