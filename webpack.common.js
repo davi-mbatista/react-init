3c17720c2c6c10c2a38705a97481ea9c9f914172
Daví Machado <machadobatista.davi@gmail.com>
2017-09-21 23:50:06 -0300

    Refactor: Template #4

--- webpack.common.js
+++ webpack.common.js
@@ -7,8 +7,16 @@ module.exports = {
     entry: {
         app: path.resolve(__dirname, 'src/index.js')
     },
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: '[name].bundle.js'
+    },
     module: {
         rules: [
+            {
+                test: /\.html$/,
+                use: ['html-loader']
+            },
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
@@ -20,20 +28,80 @@ module.exports = {
             },
             {
                 test: /\.(png|svg|jpg|gif)$/,
-                exclude: /node_modules/,
                 use: ['file-loader']
             }
         ]
-    },
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: '[name].bundle.js'
     },
     plugins: [
         new webpack.optimize.ModuleConcatenationPlugin(),
         new CleanWebpackPlugin([path.resolve(__dirname, 'build')]),
         new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'public/index.html')
+            template: require('html-webpack-template'),
+            inject: false,
+            title: 'React App',
+            favicon: path.resolve(__dirname, 'public/favicon.ico'),
+            hash: true,
+            appMountId: 'root',
+            links: [
+                {
+                    href: path.resolve(__dirname, 'public/favicon-32x32.png'),
+                    rel: 'icon',
+                    sizes: '32x32',
+                    type: 'image/png'
+                },
+                {
+                    href: path.resolve(__dirname, 'public/favicon-16x16.png'),
+                    rel: 'icon',
+                    sizes: '16x16',
+                    type: 'image/png'
+                }
+            ],
+            meta: [
+                {
+                    name: 'viewport',
+                    content: 'width=device-width, minimum-scale=1, initial-scale=1, user-scalable=yes'
+                },
+                {
+                    name: 'description',
+                    content: 'A React App.'
+                },
+                {
+                    name: 'theme-color',
+                    content: '#000'
+                },
+                {
+                    name: 'mobile-web-app-capable',
+                    content: 'yes'
+                },
+                {
+                    name: 'application-name',
+                    content: 'React App'
+                },
+                {
+                    name: 'apple-mobile-web-app-capable',
+                    content: 'yes'
+                },
+                {
+                    name: 'apple-mobile-web-app-status-bar-style',
+                    content: 'black-translucent'
+                },
+                {
+                    name: 'apple-mobile-web-app-title',
+                    content: 'React App'
+                },
+                {
+                    name: 'msapplication-TileColor',
+                    content: '#000'
+                },
+                {
+                    name: 'msapplication-TileImage',
+                    content: ''
+                },
+                {
+                    name: 'msapplication-tap-highlight',
+                    content: 'yes'
+                }
+            ],
         })
     ]
 };
