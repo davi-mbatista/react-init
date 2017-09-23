33f9114f835b1a02c9d756a7491b42f11c0b5151
Daví Machado <machadobatista.davi@gmail.com>
2017-09-22 22:49:58 -0300

    Update

--- webpack.dev.babel.js
+++ webpack.dev.babel.js
@@ -2,10 +2,14 @@ import path from 'path';
 import merge from 'webpack-merge';
 import common from './webpack.common.babel.js';
 
+const HOST = process.env.HOST || '0.0.0.0';
+
 module.exports = merge(common, {
     devtool: 'inline-source-map',
     devServer: {
         historyApiFallback: true,
-        contentBase: path.resolve(__dirname, 'build')
+        open: true,
+        overlay: true,
+        host: HOST
     },
 });
