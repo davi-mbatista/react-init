72fc3c380c932a281fca95c28319a4e36b5182d3
Daví Machado <machadobatista.davi@gmail.com>
2017-09-22 23:57:14 -0300

    Update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import 'normalize.css';
+import './styles.css';
 
-const App = () => <h1>React</h1>
+const App = () => <h1>React App</h1>
 
 render(
   <App />,
