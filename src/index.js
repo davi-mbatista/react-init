ac5e0c08ef09d4a5834863e8026994d7727049c0
Daví Machado <machadobatista.davi@gmail.com>
2017-09-26 16:52:51 -0300

    Add service worker register

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import 'normalize.css';
 import './styles.css';
+import registerServiceWorker from './registerServiceWorker';
 
 const App = () => <h1>React App</h1>
 
@@ -9,3 +10,5 @@ render(
   <App />,
   document.querySelector('#root')
 );
+
+registerServiceWorker();
