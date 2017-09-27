75e987a81fc715b08a7d0cdc246eee807a6945c8
Daví Machado <machadobatista.davi@gmail.com>
2017-09-26 23:36:45 -0300

    Update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import 'normalize.css';
-import './styles.css';
 import registerServiceWorker from './registerServiceWorker';
 
-const App = () => <h1>React App</h1>
+const App = () => 'React App'
 
 render(
   <App />,
