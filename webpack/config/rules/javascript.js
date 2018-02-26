342f25e874145cb2c7aa9a1871286563a1b968f6
Daví Machado Batista <machadobatista.davi@gmail.com>
2018-02-27 00:06:30 +0100

    Update JS rules

--- webpack/config/rules/javascript.js
+++ webpack/config/rules/javascript.js
@@ -1,13 +1,32 @@
+const path = require('path');
+const paths = require('../paths');
+
 module.exports = [
     {
         enforce: 'pre',
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'eslint-loader',
+        test: /\.(js|jsx)$/,
+        use: [
+            {
+                loader: require.resolve('eslint-loader'),
+                options: {
+                    eslintPath: require.resolve('eslint'),
+                    quiet: true,
+                },
+            },
+        ],
+        exclude: path.resolve(`${paths.ROOT}/node_modules/`),
+        include: paths.SRC,
     },
     {
-        test: /\.js$/,
-        exclude: /node_modules/,
+        test: /\.(js|jsx)$/,
+        exclude: path.resolve(`${paths.ROOT}/node_modules/`),
+        include: paths.SRC,
         loader: 'babel-loader',
+        options: {
+            // This is a feature of `babel-loader` for webpack (not Babel itself).
+            // It enables caching results in ./node_modules/.cache/babel-loader/
+            // directory for faster rebuilds.
+            cacheDirectory: true,
+        },
     },
 ];
