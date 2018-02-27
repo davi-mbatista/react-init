097e5cea1dc1b69002a3b5a46be6c7916e3c3341
Daví Machado Batista <machadobatista.davi@gmail.com>
2018-02-27 01:20:20 +0100

    Update

--- webpack/config/server.js
+++ webpack/config/server.js
@@ -1,10 +1,15 @@
 const paths = require('./paths');
 
+const host = process.env.HOST || '0.0.0.0';
+
 module.exports = {
     contentBase: `${paths._fragments.public}/`,
+    disableHostCheck: true,
     watchContentBase: true,
     historyApiFallback: true,
+    clientLogLevel: 'none',
+    quiet: true,
+    host: host,
     open: true,
     overlay: true,
-    disableHostCheck: true,
 };
