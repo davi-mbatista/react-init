f319d3e07510febf01bc30ee18757c7b0395ce12
Daví Machado Batista <machadobatista.davi@gmail.com>
2018-03-04 19:45:22 +0100

    Add sample component

--- src/index.js
+++ src/index.js
@@ -1,25 +1,8 @@
-import React, { Fragment, Component } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
+import Sample from './components/Sample';
 
-class App extends Component {
-    static state = {};
-
-    doStuff = async () => {
-        const request = await fetch('https://images.unsplash.com/photo-1446770145316-10a05382c470');
-        console.log(request);
-    };
-
-    render() {
-        return (
-            <Fragment>
-                <div>Hello World</div>
-                <button onClick={this.doStuff}>Action!</button>
-            </Fragment>
-        );
-    }
-}
-
-render(<App />, document.querySelector('#root'));
+render(<Sample />, document.querySelector('#root'));
 
 registerServiceWorker();
