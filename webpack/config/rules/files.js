69771a65d5fc3084cb4abd113af3d6bfe1583fb5
Daví Machado Batista <machadobatista.davi@gmail.com>
2018-04-12 22:47:59 +0200

    update fileloader config

--- webpack/config/rules/files.js
+++ webpack/config/rules/files.js
@@ -1,6 +1,13 @@
 module.exports = [
     {
         test: /\.(png|svg|jpg|gif)$/,
-        use: ['file-loader'],
+        use: [
+            {
+                loader: 'file-loader',
+                options: {
+                    name: '[name]-[hash].[ext]',
+                },
+            },
+        ],
     },
 ];
